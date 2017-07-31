df19161ab27e8487ffa23de0977b0ced9cdd763f
Duncan Marsh <dwolfm@gmail.com>
2017-07-31 13:55:32 -0700

    Man who sleep in beer keg wake up sticky.

---
+++ whiteboard-solutions/throttle.js
@@ -0,0 +1,10 @@
+const throttle = (fn, ms) => {
+  let ready = true;
+  return (...args) => {
+    if(ready){
+      ready = false
+      setTimeout(() => ready = true, ms)
+      return fn(...args)
+    }
+  }
+}
